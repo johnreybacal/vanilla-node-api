e313b43f4c912ced39943f1a8b7b3f4a11392f3d
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-18 14:01:30 +0800

    end also returns

--- src/types/response.ts
+++ src/types/response.ts
@@ -9,18 +9,15 @@ export interface Response extends ServerResponse {
 export function success(this: Response, data: any, status = 200) {
     this.statusCode = status;
     this.write(JSON.stringify(data));
-    this.end();
-    return this;
+    return this.end();
 }
 export function created(this: Response, data: any) {
     this.statusCode = 201;
     this.write(JSON.stringify(data));
-    this.end();
-    return this;
+    return this.end();
 }
 export function clientError(this: Response, error: any, status = 400) {
     this.statusCode = status;
     this.write(JSON.stringify(error));
-    this.end();
-    return this;
+    return this.end();
 }
