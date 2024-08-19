e83f419110ec2dd5ee71de74415d2aa6c6116bd8
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-19 15:15:11 +0800

    register route once

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,13 @@ import { RouteResolver } from "./routers/routeResolver";
 import { decorateRequest } from "./streams/request";
 import { decorateResponse } from "./streams/response";
 
-export const server = createServer(async (incomingMessage, serverResponse) => {
+(function () {
     const routers = registerRoutes();
     RouteResolver.instance.register(routers);
+    console.log("Routes registered");
+})();
 
+export const server = createServer(async (incomingMessage, serverResponse) => {
     const req = decorateRequest(incomingMessage);
     const res = decorateResponse(serverResponse);
 
