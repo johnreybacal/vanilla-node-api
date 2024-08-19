e9f86b9182c1bc0a2893d2f3b68b301cc5a2875a
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-19 20:58:28 +0800

    added longDelay to verify non blocking code

--- src/components/user/controller.ts
+++ src/components/user/controller.ts
@@ -1,3 +1,5 @@
+import { Request } from "@/streams/request";
+import { Response } from "@/streams/response";
 import { RestController } from "../../rest/controller.abstract";
 import { userService } from "./service";
 import { User } from "./type";
@@ -6,6 +8,12 @@ class Controller extends RestController<User> {
     constructor() {
         super(userService);
     }
+
+    longDelay = async (_req: Request, res: Response) => {
+        setTimeout(() => {
+            res.success("hehe");
+        }, 5000);
+    };
 }
 
 export const userController = new Controller();
--- src/components/user/route.ts
+++ src/components/user/route.ts
@@ -4,6 +4,7 @@ import { userController } from "./controller";
 const router = new Router("users");
 
 router.get("/", userController.index);
+router.get("/longDelay", userController.longDelay);
 router.get("/:id", userController.show);
 router.post("/", userController.create);
 router.put("/:id", userController.update);
